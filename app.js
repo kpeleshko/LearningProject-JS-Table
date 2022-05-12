3d7b624ee663c6480a8f275f216b221d7b8c2aed
CHERNOVA <kate200ka@gmail.com>
2022-05-12 11:58:56 +0200

    ADD: Drag and Drop functional

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ const inputQty = document.querySelector("[name=qty]");
 const inputAvailability = document.querySelector("[name=availability]");
 const columnHeaders = document.querySelectorAll("[data-sort-type]");
 
+let row;
+
+function start() {  
+    row = event.target; 
+}
+
+function dragover() {
+    let e = event;
+    e.preventDefault(); 
+    
+    let children= Array.from(e.target.parentNode.parentNode.children);
+    
+    if (children.indexOf(e.target.parentNode)>children.indexOf(row)) {
+        e.target.parentNode.after(row);
+    } else {
+        e.target.parentNode.before(row);
+    }
+}
+
 columnHeaders.forEach(columnHeader => {
     columnHeader.addEventListener('click', () => {
         let dataType = columnHeader.getAttribute('data-sort-type');
@@ -132,6 +151,10 @@ generateTable(table);
 function generateRow(table, element) {
     rowsNumber++;
     let row = table.insertRow();
+    row.setAttribute("draggable", "true");
+    row.setAttribute("ondragstart", "start()");
+    row.setAttribute("ondragover", "dragover()");
+
     for (key in element) {
         let cell = row.insertCell();
         let text = document.createTextNode(element[key]);
